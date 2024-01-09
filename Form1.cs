f887f5cc3d9de995e21b8ff26d40f891831b3338
PatriceHANNE <patrice.hanne@gmail.com>
2024-01-09 11:07:13 +0100

    ADD : User.txt file

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,11 @@ namespace WinForms_Program02
             }
             else
             {
-                MessageBox.Show("Merci " + nom_user.Text + " " + prenom_user.Text + " (" + email_user.Text + ")");
+                MessageBox.Show("Merci " + nom_user.Text + " " + prenom_user.Text + " (" + email_user.Text + "), votre inscription à bien été prise en compte.");
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter("Users.txt", true)) // pour créer le fichier si il n'existe pas et écrire dedans si il éxsite déjà
+                {
+                    file.WriteLine("Nom : " + nom_user.Text + " Prénom : " + prenom_user.Text + " Email : " + email_user.Text);
+                }
             }
         }
     }
